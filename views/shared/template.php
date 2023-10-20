8158cb8fcfca47071cb07cfeacd935322a7c8096
gimmeursocks <bobteen1@gmail.com>
2023-10-21 01:08:41 +0300

    css linked with relative address

--- views/shared/template.php
+++ views/shared/template.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <?php
       echo'
-      <link href="'.URL_ROOT.'\public\img\favicon.ico" rel="icon">
-      <link href="'.URL_ROOT.'\public\css\bootstrap.min.css" rel="stylesheet">
-      <script src="'.URL_ROOT.'\public\js\bootstrap.bundle.min.js"></script>
+      <link href="'.URL_SUBFOLDER.'\public\img\favicon.ico" rel="icon">
+      <link href="'.URL_SUBFOLDER.'\public\css\bootstrap.min.css" rel="stylesheet">
+      <script src="'.URL_SUBFOLDER.'\public\js\bootstrap.bundle.min.js"></script>
       ';
     ?>
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> -->
